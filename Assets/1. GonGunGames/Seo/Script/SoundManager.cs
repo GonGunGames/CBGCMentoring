658af70d9c6a4564dfc22af252ebcfbf7d7de240
Seo <wkapdlzk75@naver.com>
2024-08-07 16:31:00 +0900

    사운드 매니저 추가
    - 버튼 사운드 변경중

---
+++ Assets/1. GonGunGames/Seo/Script/SoundManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    [Header("Backgound")]
+    public AudioClip backgoundMusic;
+
+    [Header("UI_Button")]
+    public AudioClip buttonClickSound;
+
+    AudioSource backgoundMusicSource;
+    AudioSource buttonSoundSource;
+
+    public Slider bgmSlider;
+    public Slider btnSlider;
+
+
+    private void Awake()
+    {
+        backgoundMusicSource = gameObject.AddComponent<AudioSource>();
+        buttonSoundSource = gameObject.AddComponent<AudioSource>();
+
+        backgoundMusicSource.loop = true;
+    }
+
+    private void Start()
+    {
+        backgoundMusicSource.clip = backgoundMusic;
+        buttonSoundSource.clip = buttonClickSound;
+        backgoundMusicSource.Play();
+
+        
+    }
+
+    public void SetMusicVolume()
+    {
+        backgoundMusicSource.volume = bgmSlider.value;
+    }
+
+    public void SetButtonVolume()
+    {
+        buttonSoundSource.volume = btnSlider.value;
+    }
+
+
+    public void PlayClickSound()
+    {
+        buttonSoundSource.PlayOneShot(buttonClickSound);
+    }
+}
