3d759fb8bed63f90231dea6926d47440a005c24a
Seo <wkapdlzk75@naver.com>
2024-08-13 00:48:31 +0900

    Update InventoryManager.cs

--- Assets/Game System/Game System/Script/Inventory/InventoryManager.cs
+++ Assets/Game System/Game System/Script/Inventory/InventoryManager.cs
@@ -212,7 +212,7 @@ public class InventoryManager : Singleton<InventoryManager>
             }
             //int statLen = currentItem.data.currentStat.Length;
             int statLen = currentItem.data.info.baseStat.stats.Length;
-            if (statLen <= 2)
+            if (statLen <= 3)
             {
                 for (int i = 0; i < statLen; i++)
                 {
